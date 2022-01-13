bd04029c75d27519db11f51f94a9bb0f30b5e59c
Bram <janbramvanmeurs@gmail.com>
2022-01-13 10:25:37 +0100

    dots plot

--- 3d-representation.py
+++ 3d-representation.py
@@ -11,21 +11,17 @@ plt.rcParams['figure.dpi'] = 150
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-ax.set_xlim([0, 6])
+ax.set_xlim([6, 0])
 ax.set_ylim([0, 6])
 ax.set_zlim([0, 8])
 
 begin = (0, 1, 0)
 eind = (0, 2, 0)
 
-# ax.scatter(0, 1, 0)
-# ax.scatter(0, 2, 0)
+ax.scatter(0, 1, 0)
+ax.scatter(0, 2, 0)
 # plt.quiver(begin[0], begin[1], begin[2], eind[0], eind [1], eind[2], scale=1)
 ax.quiver(begin[0], begin[1], begin[2], eind[0], eind [1], eind[2], length=0.45)
-
-# x = np.random.normal(size=500)
-# y = np.random.normal(size=500)
-# z = np.random.normal(size=500)
 
 
 plt.savefig('representation.png')
