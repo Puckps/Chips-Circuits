a18597a4f7951cb89495d23b2206e8534327cf79
Bram <janbramvanmeurs@gmail.com>
2022-01-19 16:57:12 +0100

    costs

--- new_main.py
+++ new_main.py
@@ -2,31 +2,42 @@ from functions import plot_graph
 from sys import argv
 import pandas as pd
 from classes.board import Board
+from greedy import Greedy
 
 
 if len(argv) != 3:
     print("Usage: python3 filename main [gate file nr] [netist file nr]")
-gate_file = f"data/gates&netlists/chip_0/print_{argv[1]}.csv"
-net_file = f"data/gates&netlists/chip_0/netlist_{argv[2]}.csv"
+gate_file = f"data/gates&netlists/chip_{argv[1]}/print_{argv[1]}.csv"
+net_file = f"data/gates&netlists/chip_{argv[1]}/netlist_{argv[2]}.csv"
 
 board = Board(gate_file, net_file)
 
 net_list = []
 path_list = []
 length = 0
+# for i in range(3):
+#     for node in board._nodes:
+#         if node.get_coords() == (6, 5-i, 0):
+#             for neighbours in node._neighbours:
+#                 print(neighbours.get_coords())
+#             print()
+
 # add nets to the path-objects
-for path in board._paths:
-    net = path.shortest_path()
-    print(net)
+for path in board._paths[:7]:
+    #net = path.shortest_path()
+    greedy = Greedy(path)
+    greedy.run_greedy()
+    print(path._path)
+    net = path._path
     path_str = str(net).replace(" ", "")
     net_output = f"({int(path._net_gates[0].get_gate().id)},{int(path._net_gates[1].get_gate().id)})"
     net_list.append(net_output)
     path_list.append(path_str)
-    length = length + len(path._path)
-length = length - len(path_list)
+    #length = length + len(path._path)
+#length = length - len(path_list)
 
 df = pd.DataFrame({"net": net_list, "wires": path_list})
-df2 = {"net": f"chip_{argv[1]}_net_{argv[2]}", "wires" : length}
+df2 = {"net": f"chip_{argv[1]}_net_{argv[2]}", "wires" : board.calculate_costs()}
 df = df.append(df2, ignore_index=True)
 df.to_csv("output.csv", index=False)
 
