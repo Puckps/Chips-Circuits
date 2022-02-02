b9d8fe293153151146ad81f35235392667f78988
Levi <levimlnr@gmail.com>
2022-02-02 23:31:22 +0100

    pep8 friendly

--- main.py
+++ main.py
@@ -1,25 +1,20 @@
 from sys import argv
 import pandas as pd
-
-
 from code.algoritme.greedy import Greedy
 from code.algoritme.a_star_priority import A_star
 from code.algoritme.hill_climber import HillClimber
 from classes.path import Path
-
-
 from code.functions.functions import plot_graph, plot_hill_graph
 from code.functions.load import import_gates, import_net
-from code.functions.netlist_functions import *
 
 
-
-# ------------------------------------ Greedy ------------------------------------
+# --------------------------------- Greedy ------------------------------------
 # from code.algoritme.greedy import Greedy
 # from classes.board import Board
 
 # if len(argv) != 3:
 #     print("Usage: python3 filename main [chip nr] [netist file nr]")
+#     exit(0)
 
 # gate_file = f"data/gates&netlists/chip_{argv[1]}/print_{argv[1]}.csv"
 # net_file = f"data/gates&netlists/chip_{argv[1]}/netlist_{argv[2]}.csv"
@@ -38,7 +33,8 @@ from code.functions.netlist_functions import *
 
 #     net = path._path
 #     path_str = str(net).replace(" ", "")
-#     net_output = f"({int(path._net_gates[0].get_gate().id)},{int(path._net_gates[1].get_gate().id)})"
+#     net_output = f"({int(path._net_gates[0].get_gate().id)},"\
+#                  f"{int(path._net_gates[1].get_gate().id)})"
 #     net_list.append(net_output)
 #     path_list.append(path_str)
 
@@ -52,14 +48,13 @@ from code.functions.netlist_functions import *
 # print()
 
 
-
-
-# ------------------------------------ A* ------------------------------------
+# ------------------------------------ A* -------------------------------------
 # from code.algoritme.a_star_priority import A_star
 # from classes.board import Board
 
 # if len(argv) != 3:
 #     print("Usage: python3 filename main [chip nr] [netist file nr]")
+#     exit(1)
 
 # gate_file = f"data/gates&netlists/chip_{argv[1]}/print_{argv[1]}.csv"
 # net_file = f"data/gates&netlists/chip_{argv[1]}/netlist_{argv[2]}.csv"
@@ -78,7 +73,8 @@ from code.functions.netlist_functions import *
 
 #     net = path._path
 #     path_str = str(net).replace(" ", "")
-#     net_output = f"({int(path._net_gates[0].get_gate().id)},{int(path._net_gates[1].get_gate().id)})"
+#     net_output = f"({int(path._net_gates[0].get_gate().id)},"\
+#                  f"{int(path._net_gates[1].get_gate().id)})"
 #     net_list.append(net_output)
 #     path_list.append(path_str)
 
@@ -92,13 +88,13 @@ from code.functions.netlist_functions import *
 # print()
 
 
-
-
-# ------------------------------------ Hill Climber ------------------------------------
+# -------------------------------- Hill Climber -------------------------------
 
 
 if len(argv) != 5:
-    print("Usage: python3 filename main [chip nr] [netist file nr] [nr of restarts] [nr of reverts before restart]")
+    print("Usage: python3 filename main [chip nr] [netist file nr]",
+          "[nr of restarts] [nr of reverts before restart]")
+    exit(2)
 
 gate_file = f"data/gates&netlists/chip_{argv[1]}/print_{argv[1]}.csv"
 net_file = f"data/gates&netlists/chip_{argv[1]}/netlist_{argv[2]}.csv"
@@ -123,27 +119,30 @@ path_list = []
 for path in board._paths:
     net = path._path
     path_str = str(net).replace(" ", "")
-    net_output = f"({int(path._net_gates[0].get_gate().id)},{int(path._net_gates[1].get_gate().id)})"
+    net_output = f"({int(path._net_gates[0].get_gate().id)},"\
+                 f"{int(path._net_gates[1].get_gate().id)})"
     net_list.append(net_output)
     path_list.append(path_str)
 
-best_df = pd.DataFrame({"netlist": [(hill_list)],"score": [costs[2]], "intersections": [costs[1]], "restarts": [argv[3]], "max_reverts": [argv[4]]})
+best_df = pd.DataFrame({"netlist": [(hill_list)], "score": [costs[2]],
+                        "intersections": [costs[1]], "restarts": [argv[3]],
+                        "max_reverts": [argv[4]]})
 
 with open(f"output/chip_{argv[1]}/output_netlist_{argv[2]}.csv", 'a') as f:
     best_df.to_csv(f, header=False, index=False)
 
 
-
-# ------------------------------------ Output ------------------------------------
+# --------------------------------- Output ------------------------------------
 
 df = pd.DataFrame({"net": net_list, "wires": path_list})
-df2 = {"net": f"chip_{argv[1]}_net_{argv[2]}", "wires" : costs[2]}
+df2 = {"net": f"chip_{argv[1]}_net_{argv[2]}", "wires": costs[2]}
 df = df.append(df2, ignore_index=True)
 df.to_csv("output/output.csv", index=False)
 
-plot_graph(board._nodes, board._paths, board._dimensions[0], board._dimensions[1])
+plot_graph(board._nodes, board._paths,
+           board._dimensions[0], board._dimensions[1])
 
-# ------------------------- Generate hill climber graph --------------------------
+# ------------------------ Generate hill climber graph ------------------------
 
 # graph_costs = results[3]
 # plot_hill_graph(graph_costs)
